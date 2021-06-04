5f7b8ceb0b6d7aebf7f2a2eadc04fb7ec4d81bce
DanteAlonsoHT <dante1818@hotmail.com>
2021-06-04 11:58:25 -0500

    Fix create method in posts_controller.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :content, presence: true, length: { minimum: 10 }
 end
