afd7f60141261fc042ef8ec1cf24ff531b8fc506
Daniel Samuel <dansam5000@gmail.com>
2021-06-04 23:08:52 +0100

    Add precompile to Application

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +21,6 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
   end
 end
