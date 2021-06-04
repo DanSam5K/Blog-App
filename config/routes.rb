bf104dcc07e24dbe153a00b07573a9e0431e8590
Daniel Samuel <dansam5000@gmail.com>
2021-06-04 05:05:17 +0100

    Add posts controller and few files changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  resources :blog
+  root "home#index"
+  resources :posts
+
+  get '/home', to: 'home#index'
+  get '/posts', to: 'posts#index'
 end
