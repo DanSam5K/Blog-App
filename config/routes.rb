409e99c7fed3352de2ff9b610fab07ea4cc128f4
Daniel Samuel <dansam5000@gmail.com>
2021-06-04 15:14:43 +0100

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
+
   resources :posts
 
+  resources :posts do
+    resources :comments
+  end
+
   # get '/home', to: 'home#index'
-  # get '/posts', to: 'posts#index'
+  get '/posts', to: 'posts#index'
 end
